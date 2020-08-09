bf19215ed13b5c2cff1b0397108f4196df1da655
Matthew-swin <103219809@swin.edu.au>
2020-08-10 09:54:17 +1000

    task1 complete

--- index.sql
+++ index.sql
@@ -1,3 +1,26 @@
 --Matthew Jester
 --103219809
 
+-- Tour (TourName, Description)
+-- PK(TourName)
+
+-- Client (ClientID, Surname, GivenName, Gender)
+-- PK (ClientID)
+
+-- Event (TourName, EventDay, EventMonth, EventYear, Fee)
+-- PK (TourName, EventDay, EventMonth, EventYear)
+-- FK (TourName) References Tour
+
+-- Booking (Tourname, EventDay, EventMonth, EventYear, ClientID, DateBooked, Payment)
+-- PK (Tourname, EventDay, EventMonth, EventYear)
+-- FK (TourName) references Tour
+-- FK (EventDay, EventMonth, EventYear) References Tour
+-- FK(ClientID) References Client
+
+CREATE DATABASE Test;
+
+SELECT *  
+FROM sys.databases ;  
+
+USE Test;
+ 
