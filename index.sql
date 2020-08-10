4d33df895f077917c52f59fd60da96fdc007a9a3
Matthew-swin <103219809@swin.edu.au>
2020-08-10 10:55:31 +1000

    Task 2 Complete

--- index.sql
+++ index.sql
@@ -23,4 +23,61 @@ SELECT *
 FROM sys.databases ;  
 
 USE Test;
- 
+
+--dropping if mess up
+IF OBJECT_ID('Event') IS NOT NULL
+DROP TABLE Event;
+
+IF OBJECT_ID('Booking') IS NOT NULL
+DROP TABLE Booking;
+
+IF OBJECT_ID('Tour') IS NOT NULL
+DROP TABLE Tour;
+
+IF OBJECT_ID('Client') IS NOT NULL
+DROP TABLE Client;
+
+--Creating tables
+GO;
+
+CREATE TABLE Client (
+    ClientId    INT
+,   Surname     NVARCHAR(100) NOT NULL
+,   GivenName   NVARCHAR(100) NOT NULL
+,   Gender      NVARCHAR(1) CHECK (Gender IN ('M','F','I')) NULL
+, PRIMARY KEY (ClientId)
+);
+
+CREATE TABLE Tour (
+    TourName     NVARCHAR(100)
+,   Description  NVARCHAR(500) 
+, PRIMARY KEY (TourName)
+);
+
+CREATE TABLE Event (
+    TourName     NVARCHAR(100)
+,   EventDay    INT CHECK (EventDay >=1 and EventDay <= 31)
+,   EventMonth  NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May','Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+,   EventYear   INT CHECK (len(EventYear) = 4)
+,   EventFee    MONEY NOT NULL check (EventFee > 0)
+, PRIMARY KEY (TourName, EventDay, EventMonth, EventYear)
+, FOREIGN KEY (TourName) REFERENCES Tour
+);
+
+CREATE TABLE Booking (
+    ClientId    INT
+,   TourName     NVARCHAR(100)
+,   EventDay    INT CHECK (EventDay >=1 and EventDay <= 31)
+,   EventMonth  NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May','Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+,   EventYear   INT CHECK (len(EventYear) = 4)
+,   Payment     MONEY CHECK (Payment > 0) 
+,   DateBooked  DATE NOT NULL
+, PRIMARY KEY (ClientID, TourName, EventDay, EventMonth, EventYear)
+, FOREIGN KEY (TourName, EventDay, EventMonth, EventYear) REFERENCES Event
+, FOREIGN KEY (ClientID) REFERENCES Client
+);
+
+GO;
+
+select *
+from  sys.tables
